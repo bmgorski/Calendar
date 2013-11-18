6e3cd3f8f3734a245754e69b8f19b810b2be118d
Brian M Gorski <bmgorski987+github@gmail.com>
2013-11-17 20:04:51 -0600

    Fixed task implimentation.

--- Calendar/src/main/java/com/briangorski/calendar/model/DefaultTask.java
+++ Calendar/src/main/java/com/briangorski/calendar/model/DefaultTask.java
@@ -3,29 +3,42 @@ package com.briangorski.calendar.model;
 import java.util.Date;
 
 public class DefaultTask implements Task {
-	private String description;
+	private Long taskID;
 	private Date dueDate;
-	
+	private String description;
+
+	@Override
+	public void setTaskID(Long taskID) {
+		this.taskID = taskID;
+	}
+
+	@Override
+	public Long getTaskID() {
+		return this.taskID;
+	}
+
+	@Override
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
 	@Override
 	public Date dueDate() {
 		return this.dueDate;
 	}
 
 	@Override
-	public String description() {
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String getDescription() {
 		return this.description;
 	}
 
 	@Override
 	public String type() {
 		return "Default Type";
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void setDueDate(Date dueDate) {
-		this.dueDate = dueDate;
 	}
 }
--- Calendar/src/main/java/com/briangorski/calendar/model/Task.java
+++ Calendar/src/main/java/com/briangorski/calendar/model/Task.java
@@ -3,7 +3,14 @@ package com.briangorski.calendar.model;
 import java.util.Date;
 
 public interface Task {
+	void setTaskID(Long taskID);
+	Long getTaskID();
+	
+	void setDueDate(Date dueDate);
 	Date dueDate();
-	String description();
+	
+	void setDescription(String description);
+	String getDescription();
+	
 	String type();
 }
